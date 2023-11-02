bc6dbbdb53cfb5ffca0635ea700bfb0fccb7cb7b
ThomyRa <tjramirezr@gmail.com>
2023-11-01 20:12:41 -0500

    Feat: OOP Coffee Machine

--- Day016/Coffee_Machine/oop_coffee_machine.py
+++ Day016/Coffee_Machine/oop_coffee_machine.py
@@ -1,39 +1,23 @@
-class MoneyMachine:
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-    CURRENCY = "$"
 
-    COIN_VALUES = {
-        "quarters": 0.25,
-        "dimes": 0.10,
-        "nickles": 0.05,
-        "pennies": 0.01
-    }
+coffee_menu = Menu()
+coffee_maker = CoffeeMaker()
+machine_menu = Menu()
+money_transaction = MoneyMachine()
 
-    def __init__(self):
-        self.profit = 0
-        self.money_received = 0
-
-    def report(self):
-        """Prints the current profit"""
-        print(f"Money: {self.CURRENCY}{self.profit}")
-
-    def process_coins(self):
-        """Returns the total calculated from coins inserted."""
-        print("Please insert coins.")
-        for coin, value in self.COIN_VALUES.items():
-            self.money_received += int(input(f"How many {coin}?: ")) * value
-        return self.money_received
-
-    def make_payment(self, cost):
-        """Returns True when payment is accepted, or False if insufficient."""
-        self.process_coins()
-        if self.money_received >= cost:
-            change = round(self.money_received - cost, 2)
-            print(f"Here is {self.CURRENCY}{change} in change.")
-            self.profit += cost
-            self.money_received = 0
-            return True
-        else:
-            print("Sorry that's not enough money. Money refunded.")
-            self.money_received = 0
-            return False
+is_on = True
+while is_on:
+    user_choice = input(f"What would you like? ({coffee_menu.get_items()}): ")
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        coffee_maker.report()  
+        money_transaction.report() 
+    else:
+        user_order = machine_menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(user_order) and \
+            money_transaction.make_payment(user_order.cost):
+            coffee_maker.make_coffee(user_order)
