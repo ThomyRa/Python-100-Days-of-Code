647c041d34138efd3af75cf1acc96d49dd0ede5d
ThomyRa <tjramirezr@gmail.com>
2023-11-15 11:45:44 -0500

    Feat: Organize and clean Mail Marge Project

--- Day024/Mail Merge Project Start/main.py
+++ Day024/Mail Merge Project Start/main.py
@@ -1,12 +1,11 @@
 
-with open("../Mail Merge Project Start/Input/Names/invited_names.txt") as file:
+with open("./Input/Names/invited_names.txt") as file:
     all_names = file.readlines()
 all_names = [name.strip() for name in all_names]
 
 for name in all_names:
-    with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt") as file:
+    with open("./Input/Letters/starting_letter.txt") as file:
         starting_letter = file.readlines()
-    with open(f"../Mail Merge Project Start/Output/ReadyToSend/{name}.txt", "w") as file:
+    with open(f"./Output/ReadyToSend/{name}.txt", "w") as file:
         starting_letter[0] = starting_letter[0].replace("[name]", name)
-        print(starting_letter[0])
         file.writelines(starting_letter)
