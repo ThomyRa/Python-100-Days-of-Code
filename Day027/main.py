a27d7df2b90f37198801c356027da85d79dd2ba3
ThomyRa <tjramirezr@gmail.com>
2023-11-21 12:55:38 -0500

    Feat: Tkinter Layout Managers

--- Day027/main.py
+++ Day027/main.py
@@ -1,29 +1,31 @@
 from tkinter import *
 
+
+def button_clicked():
+    text = my_input.get()
+    label.config(text=text)
+
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
-my_label = Label(text="I'm a label",
-                 font=("Arial", 24, "bold"))
-my_label.pack()
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
+label = Label(text="I'm a label",
+              font=("Arial", 24, "bold"))
+label.config(text="New Text")
+label.grid(row=0, column=0)
 
-###################################################
-# Button
-def button_clicked():
-    text = my_input.get()
-    my_label.config(text=text)
+button1 = Button(text="Button 1", command=button_clicked)
+button1.grid(row=1, column=1)
 
-
-my_button = Button(text="Click Me", command=button_clicked)
-my_button.pack()
+button2 = Button(text="Button 2", command=button_clicked)
+button2.grid(row=0, column=2)
 
 # Entry
 my_input = Entry(width=10)
-my_input.pack()
+my_input.grid(row=2, column=3)
 
 
 window.mainloop()
