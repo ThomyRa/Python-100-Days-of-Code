21c00cdaf2d09a99cb27baa6cb4d905c37ca4821
ThomyRa <tjramirezr@gmail.com>
2023-12-27 21:04:49 -0500

    Feat: Stock Trading News Alert - Getting difference

--- Day036/stock-news-hard-start/main.py
+++ Day036/stock-news-hard-start/main.py
@@ -22,12 +22,18 @@ parameters = {
 response = requests.get(url=STOCK_ENDPOINT, params=parameters)
 response.raise_for_status()
 data = response.json()
-# pprint(data)
 data_list = [value for (key, value) in data['Time Series (Daily)'].items()]
+
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data['4. close']
 
 before_yesterday_data = data_list[1]
 before_yesterday_closing_price = before_yesterday_data['4. close']
 
-print(yesterday_closing_price, before_yesterday_closing_price)
+difference = abs(float(yesterday_closing_price) - float(before_yesterday_closing_price))
+print(difference)
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+print(diff_percent)
+
+if diff_percent > 5:
+    print("Get News")
