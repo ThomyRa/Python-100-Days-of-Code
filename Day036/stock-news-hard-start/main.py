036d3a994dbc59ed643836cde0854697fd5eaa57
ThomyRa <tjramirezr@gmail.com>
2023-12-27 21:58:49 -0500

    Feat: Stock Trading News Alert - Fetching news artcles

--- Day036/stock-news-hard-start/main.py
+++ Day036/stock-news-hard-start/main.py
@@ -11,18 +11,19 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 load_dotenv()
 STOCK_API_KEY = os.getenv("STOCK_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
-parameters = {
+stock_parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
     "outputsize": "compact",
     "apikey": STOCK_API_KEY
 }
 
-response = requests.get(url=STOCK_ENDPOINT, params=parameters)
-response.raise_for_status()
-data = response.json()
-data_list = [value for (key, value) in data['Time Series (Daily)'].items()]
+stock_response = requests.get(url=STOCK_ENDPOINT, params=stock_parameters)
+stock_response.raise_for_status()
+stock_data = stock_response.json()
+data_list = [value for (key, value) in stock_data['Time Series (Daily)'].items()]
 
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data['4. close']
@@ -35,5 +36,13 @@ print(difference)
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
-if diff_percent > 5:
-    print("Get News")
+if diff_percent > 1:
+    news_parameters = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
+    news_response = requests.get(url=NEWS_ENDPOINT, params=news_parameters)
+    news_response.raise_for_status()
+    news_data = news_response.json()
+    articles = news_data['articles']
+    pprint(articles[:3])
