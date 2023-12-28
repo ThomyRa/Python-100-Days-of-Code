72e09d0e5e7b218fe5eca8dbc2abf4c638488225
ThomyRa <tjramirezr@gmail.com>
2023-12-28 16:54:35 -0500

    Feat: Rain Alert App - Refactored

--- Day035/Rain_Alert/main.py
+++ Day035/Rain_Alert/main.py
@@ -5,15 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 OPW_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = os.getenv("OPW_API_KEY")
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+API_KEY = os.getenv("OPW_API_KEY")
+ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TO_PHONE = os.getenv('TO_PHONE')
 
 
 parameters = {
     "lat": 5.067680,
     "lon": -75.509819,
-    "appid": api_key,
+    "appid": API_KEY,
     "units": "metric",
     "cnt": 4
 }
@@ -29,12 +30,12 @@ for hour_data in weather_data["list"]:
         will_rain = True
 
 if will_rain:
-    client = Client(account_sid, auth_token)
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
     message = client.messages \
         .create(
                 body="It's going to rain. Remeber to bring and umbrella.☂️",
                 from_='+19282385222',
-                to='+573028548550'
+                to=TO_PHONE
         )
     print(message.status)
 
