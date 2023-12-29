9b781660ba7c965dd7ddacdb599bf203940a3090
ThomyRa <tjramirezr@gmail.com>
2023-12-29 16:14:52 -0500

    Feat: Habit Tracker - Creating a pixel

--- Day037/Habit_Tracker/main.py
+++ Day037/Habit_Tracker/main.py
@@ -1,32 +1,52 @@
 import os
-
 import requests
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 TOKEN = os.getenv("TOKEN")
 USERNAME = os.getenv("USERNAME")
-user_parameters = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
-# response = requests.post(url=PIXELA_ENDPOINT, json=user_parameters)
-# print(response.text)
-
-graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+GRAPH_ID = "graph01"
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-graph_config = {
-    "id": "graph01",
-    "name": "Meditation Graph",
-    "unit": "Mins",
-    "type": "int",
-    "color": "sora"
+####################################################
+# CREATING USER
+
+# user_parameters = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes"
+# }
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_parameters)
+# print(response.text)
+
+
+####################################################
+# CREATING GRAPH
+
+# graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+# graph_config = {
+#     "id": GRAPH_ID,
+#     "name": "Meditation Graph",
+#     "unit": "Mins",
+#     "type": "int",
+#     "color": "sora"
+# }
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+####################################################
+# ADDING PIXEL
+
+pixel_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+today = datetime.datetime.now()
+pixel_params = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "20",
 }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
 print(response.text)
