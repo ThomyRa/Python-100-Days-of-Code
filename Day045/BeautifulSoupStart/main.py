1da96c69fa1c5069f6278ac3c8669c5ec775cde5
ThomyRa <tjramirezr@gmail.com>
2024-01-16 18:57:53 -0500

    Feat: Beautiful Soup - Selecting Particular Elements

--- Day045/BeautifulSoupStart/main.py
+++ Day045/BeautifulSoupStart/main.py
@@ -9,7 +9,30 @@ soup = BeautifulSoup(htmlContents, "html.parser")
 # print(soup.title.string)
 # print(soup.prettify())
 
-print(soup.a)
-print(soup.p)
+# print(soup.a)
+# print(soup.p)
 
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+# all_p_tags = soup.find_all(name="p")
 
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+#     print(tag.get("href"))
+
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.getText())
+# print(section_heading.name)
+# print(section_heading.get("class"))
+
+company_url = soup.select_one("p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select(".heading")
+print(headings)
