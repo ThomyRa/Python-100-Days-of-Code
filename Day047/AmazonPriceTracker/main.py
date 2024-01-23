fb2768bcb6b8a8cae8edaa099bd8c6a36704b63e
ThomyRa <tjramirezr@gmail.com>
2024-01-23 11:15:50 -0500

    Feat: Amazon Price Tracker

--- Day047/AmazonPriceTracker/main.py
+++ Day047/AmazonPriceTracker/main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 from prettyprinter import pprint
 import lxml
+import smtplib
+from dotenv import load_dotenv
+import os
 
 URL = "https://www.amazon.com/BINNUNE-Microphone-Playstation-Headphones-Cancelling/dp/B096VRK6J5/133-4732069-7769535"
 headers = {
@@ -10,8 +13,21 @@ headers = {
 }
 response = requests.get(url=URL, headers=headers)
 soup = BeautifulSoup(response.content, "lxml")
-# pprint(soup)
 
-price = soup.find(class_="a-offscreen").getText().split("$")[1]
+price = float(soup.find(class_="a-offscreen").getText().split("$")[1])
 pprint(price)
 
+if price <= 35:
+    load_dotenv()
+    message = f"Subject:Alert Low Price🚨🏷️\nLow price in this product on Amazon:\n\n{URL}"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(
+            user=os.getenv("MY_EMAIL"),
+            password=os.getenv("PASSWORD"),
+        )
+        connection.sendmail(
+            from_addr=os.getenv("MY_EMAIL"),
+            to_addrs=os.getenv("MY_EMAIL"),
+            msg=message.encode("utf8")
+        )
