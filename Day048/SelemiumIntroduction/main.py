18129e250fce9571664643dd93810ccdae57c939
ThomyRa <tjramirezr@gmail.com>
2024-01-24 12:04:34 -0500

    Feat: Selenium Introduction

---
+++ Day048/SelemiumIntroduction/main.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+##############################
+# Day 47 Challenge solved using Selenium
+URL = "https://www.amazon.com/BINNUNE-Microphone-Playstation-Headphones-Cancelling/dp/B096VRK6J5/133-4732069-7769535"
+driver.get(url=URL)
+
+price_dollar = driver.find_element(By.CLASS_NAME, "a-price-whole")
+price_cents = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+
+print(f"The price is {price_dollar.text}.{price_cents.text}")
+
+# Closes a single Tab
+# driver.close()
+
+# Closes the entire Browser
+driver.quit()
+##############################
+
+
