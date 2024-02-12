441151d103014835887e77637f107a6e3ac75c51
ThomyRa <tjramirezr@gmail.com>
2024-02-12 09:13:06 -0500

    Feat: Flask - URL Paths

---
+++ Day055/URL_Paths/main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello World!</h1"
+
+
+@app.route("/bye")
+def say_bye():
+    return "<h1>Bye</h1>"
+
+
+@app.route("/username/<name>")
+def greet(name):
+    return f"Hello {name}!!"
+
+
+# To run the Flask app from IDE
+if __name__ == "__main__":
+    app.run()
