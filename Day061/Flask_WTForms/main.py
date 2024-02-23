efeeb5afb4af9b8385287e22cb1d807029b4a149
ThomyRa <tjramirezr@gmail.com>
2024-02-22 21:29:58 -0500

    Feat: Flask-WTForms Receiving Data

--- Day061/Flask_WTForms/main.py
+++ Day061/Flask_WTForms/main.py
@@ -21,17 +21,19 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
-    return render_template("success.html", form=login_form)
-
-
-# @app.route("/logout.html")
-# def logout():
-#     session.clear()
-#     return render_template("logout.html")
+    if login_form.validate_on_submit:
+        print(login_form.email.data)
+        # if (
+        #     login_form.email.data == "admin@email.com"
+        #     and login_form.password.data == "qwerty"
+        # ):
+        #     return render_template("success.html")
+        # else:
+        #     return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == "__main__":
