df5898fb1b08aee99e6e5d4cb1841e0419f74d7d
ThomyRa <tjramirezr@gmail.com>
2024-02-23 17:31:06 -0500

    Feat: Flask-WTForms Access Denied

--- Day061/Flask_WTForms/main.py
+++ Day061/Flask_WTForms/main.py
@@ -26,13 +26,11 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit:
         print(login_form.email.data)
-        # if (
-        #     login_form.email.data == "admin@email.com"
-        #     and login_form.password.data == "qwerty"
-        # ):
-        #     return render_template("success.html")
-        # else:
-        #     return render_template("denied.html")
+        if login_form.email.data == "admin@email.com" \
+                and login_form.password.data == "qwerty":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
