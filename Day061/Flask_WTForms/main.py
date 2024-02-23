20c45fd28186e45eed34c7092203c204a714262b
ThomyRa <tjramirezr@gmail.com>
2024-02-22 19:13:28 -0500

    Feat: Flask-WTForms Error Handling

--- Day061/Flask_WTForms/main.py
+++ Day061/Flask_WTForms/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
@@ -24,7 +24,14 @@ def home():
 @app.route("/login")
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template("success.html", form=login_form)
+
+
+# @app.route("/logout.html")
+# def logout():
+#     session.clear()
+#     return render_template("logout.html")
 
 
 if __name__ == "__main__":
