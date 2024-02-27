71f8fb9089261d84983ac2a0d3205f6a6c09b532
ThomyRa <tjramirezr@gmail.com>
2024-02-27 17:09:25 -0500

    Feat: Databases with SQLite and SQLAlchemy

--- Day063/Databases_with_SQLite_and_SQLAlchemy/main.py
+++ Day063/Databases_with_SQLite_and_SQLAlchemy/main.py
@@ -1,8 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 from prettyprinter import pprint
+import pdb
 
 
 app = Flask(__name__)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+current_path = os.getcwd()
+db_path = f"{current_path}/books_collection.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, unique=False, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 all_books = []
 
@@ -15,12 +43,15 @@ def home():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        all_books.append({
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"],
-        })
-        pprint(all_books)
+        with app.app_context():
+            new_book = Book(
+                title=request.form["title"],
+                author=request.form["author"],
+                rating=request.form["rating"]
+            )
+            db.session.add(new_book)
+            db.session.commit()
+        return redirect(url_for('home'))
     return render_template('add.html')
 
 
