02d29aa1fc6061189794d7cffd4a2cf4b1444cfb
ThomyRa <tjramirezr@gmail.com>
2024-02-27 08:43:29 -0500

    Feat: Databases Project Init

---
+++ Day063/Databases_with_SQLite_and_SQLAlchemy/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+from prettyprinter import pprint
+
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
