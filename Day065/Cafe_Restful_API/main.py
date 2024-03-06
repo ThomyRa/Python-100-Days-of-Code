611974e9ffe0cd886c5045e71371f4dbf02050de
ThomyRa <tjramirezr@gmail.com>
2024-03-06 18:16:03 -0500

    Feat: CafeAPI RESTful Routing - Automatic Serializing

--- Day065/Cafe_Restful_API/main.py
+++ Day065/Cafe_Restful_API/main.py
@@ -34,6 +34,19 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    # Automatic Building the Response
+    def to_dict(self):
+        # Method 1
+        # Looping through each column in the data record
+        # dictionary = {}
+        # for column in self.__table__.columns:
+        #     dictionary[column.name] = getattr(self, column.name)
+        # return dictionary
+
+        # Method 2
+        # Dictionary comprehension for Method 1
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -48,21 +61,25 @@ def home():
 @app.route("/random")
 def get_random_cafe():
     random_cafe = db.session.execute(db.select(Cafe).order_by(func.random()).limit(1)).scalar()
-    return jsonify(
-        cafe={
-            "id": random_cafe.id,
-            "name": random_cafe.name,
-            "map_url": random_cafe.map_url,
-            "img_url": random_cafe.img_url,
-            "location": random_cafe.location,
-            "has_sockets": random_cafe.has_sockets,
-            "has_toilet": random_cafe.has_toilet,
-            "has_wifi": random_cafe.has_wifi,
-            "can_take_calls": random_cafe.can_take_calls,
-            "seats": random_cafe.seats,
-            "coffee_price": random_cafe.coffee_price,
-        }
-    )
+    return random_cafe.to_dict()
+
+    # Manually Building the Response
+    # return jsonify(
+    #     cafe={
+    #         # "id": random_cafe.id,
+    #         "name": random_cafe.name,
+    #         "map_url": random_cafe.map_url,
+    #         "img_url": random_cafe.img_url,
+    #         "location": random_cafe.location,
+    #         "amenities": {
+    #             "has_sockets": random_cafe.has_sockets,
+    #             "has_toilet": random_cafe.has_toilet,
+    #             "has_wifi": random_cafe.has_wifi,
+    #             "can_take_calls": random_cafe.can_take_calls,
+    #             "seats": random_cafe.seats,
+    #             "coffee_price": random_cafe.coffee_price,
+    #         }
+    #     })
 
 # HTTP POST - Create Record
 
